ea5a0f1f184fa8f929c0b686440f75e17a3555d1
Jonathan Tsang <chewymeister88@googlemail.com>
2013-11-13 19:06:00 +0000

    First red test middleware_spec

---
+++ Gemfile
@@ -0,0 +1,6 @@
+# A sample Gemfile
+source "https://rubygems.org"
+
+gem 'rack', '~> 1.4'
+gem 'simple_oauth'
+gem 'rspec', '~> 2.7'
---
+++ spec/lib/forcefield/middleware_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+describe Forcefield::Middleware do 
+  let(:death_star) { lambda { |env| [200,{},[]]} }
+  let(:middleware) { Forcefield::Middlware.new(death_star) }
+  let(:mock_request) { Rack::MockRequest.new(middleware) }
+
+  context "When incoming request has no Authorization header" do
+    let(:resp) { mock_request.get("/") }
+
+    it "returns a 401" do
+      expect(resp.status).to eq 401
+    end
+
+    it "notifies the client they are Unauthorized" do
+      expect(resp.body).to eq "Unauthorized! You are part of the Rebel Alliance and are a Traitor!"
+    end
+  end
+end
